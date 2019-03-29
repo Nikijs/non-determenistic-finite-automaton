c91637deca31a6b2816b9731a0f798a5880b7b08
Nikijs <nikijs299@gmail.com>
2019-03-29 23:58:29 +0000

    Added comments for shunting algorythm

--- NFA.py
+++ NFA.py
@@ -1,28 +1,34 @@
 #NFA
 
 def shuntingAlg(infix):
+    #special characters with precedence
     specials = {'*': 50, '.': 40, '|': 30}
 
     stack = ""
+    #operator stack
     pofix = ""
-    
+    #loop through the characters
     for c in infix:
+        #push to the stack
         if c == '(':
             stack = stack + c
+            #pop from stack
         elif c == ')':
             while stack[-1] != '(':
                 pofix, stack = pofix + stack[-1], stack[:-1]
             stack = stack[:-1]
+            #if operator push to stack after popping lower or equal precedence
         elif c in specials:
             while stack and specials.get(c, 0) <= specials.get(stack[-1], 0):
                 pofix, stack = pofix + stack[-1], stack[:-1]
             stack = stack + c
+            #regular characters pushed to stack
         else:
             pofix = pofix + c
-
+            
     while stack:
         pofix, stack = pofix + stack[-1], stack[:-1]
-             
+             #return postfix regex
     return pofix
 
 
@@ -44,7 +50,7 @@ def compile(pofix):
 
     for c in pofix:
         if c == '.':
-
+                nfa2
         elif c == '|'
 
         elif c == '*'
